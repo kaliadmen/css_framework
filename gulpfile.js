5c1ccc21a2fc2a6f6eb063a161182014416975e0
kaliadmen <kaliadmen@gmail.com>
2022-07-02 11:24:50 -0400

    ADD CSS purge and rename to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
+const rename = require('gulp-rename');
 
 function buildStyles(){
     return src('dragon_spider/**/*.scss')
+        //compile css
         .pipe(sass())
+        //rename output file
+        .pipe(rename('styles.css'))
+        //purge unused css
+        .pipe(purgecss({content: ['*.html']}))
+        //put css file in destination
         .pipe(dest('css'))
 }
 
 function watchTask(){
-    watch(['dragon_spider/**/*.scss'], buildStyles)
+    watch(['dragon_spider/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
